f4291cf972a7d80b6ffd71f74296cbc0906d9ce9
Dafa Bintang Athaillah <55053091+Dafaathaillah@users.noreply.github.com>
2022-12-07 10:35:48 +0800

    add dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +21,25 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        // ]);
+
+        User::create([
+            "name" => "Dafa",
+            "email" => "Dafa@mail.com",
+            "password" => Hash::make('12345678'),
+        ]);
+
+        User::create([
+            "name" => "Dafa",
+            "email" => "Dafa1@mail.com",
+            "password" => Hash::make('12345678'),
+        ]);
+
+        // User::insert([
+        //     "id_user" => "1",
+        //     "name" => "Dafa",
+        //     "email" => "Dafa1@mail.com",
+        //     "password" => Hash::make('12345678'),
         // ]);
     }
 }
